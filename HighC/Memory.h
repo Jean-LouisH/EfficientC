07bd410247fe7dead164be6d274db4f64d19a0c8
Luleivon <Louis_Ml@live.co.uk>
2018-10-15 03:07:21 -0400

    Renamed Binary to Memory

--- HighC/Memory.h
+++ HighC/Memory.h
@@ -1,60 +1,60 @@
-#ifndef Binary_h
-#define Binary_h
+#ifndef Memory_h
+#define Memory_h
 
 #include <stdint.h>
 
-//Unsigned binaries
+//Unsigned
 //////////////////////////////////
 
 typedef struct
 {
 	uint8_t* data;
 	uint64_t size;
-}HighC_Binary_u8;
+}HighC_Memory_u8;
 
 typedef struct
 {
 	uint16_t* data;
 	uint64_t size;
-}HighC_Binary_u16;
+}HighC_Memory_u16;
 
 typedef struct
 {
 	uint32_t* data;
 	uint64_t size;
-}HighC_Binary_u32;
+}HighC_Memory_u32;
 
 typedef struct
 {
 	uint64_t* data;
 	uint64_t size;
-}HighC_Binary_u64;
+}HighC_Memory_u64;
 
-//Signed binaries
+//Signed
 //////////////////////////////////
 
 typedef struct
 {
 	int8_t* data;
 	uint64_t size;
-}HighC_Binary_s8;
+}HighC_Memory_s8;
 
 typedef struct
 {
 	int16_t* data;
 	uint64_t size;
-}HighC_Binary_s16;
+}HighC_Memory_s16;
 
 typedef struct
 {
 	int32_t* data;
 	uint64_t size;
-}HighC_Binary_s32;
+}HighC_Memory_s32;
 
 typedef struct
 {
 	int64_t* data;
 	uint64_t size;
-}HighC_Binary_s64;
+}HighC_Memory_s64;
 
-#endif /*Binary_h*/
+#endif /*Memory_h*/
